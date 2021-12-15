fd61e6f46a89772248447c1c12e8d5d34c71a4df
Tijmen de Mes <t.w.k.demes@concepts.nl>
2021-12-15 12:43:02 +0100

    Added option to specify a custom regex

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const createHtmlElement = require('create-html-element');
 
 // Capture the whole URL in group 1 to keep string.split() support
-const urlRegex = () => (/((?:https?(?::\/\/))(?:www\.)?(?:[a-zA-Z\d-_.]+(?:(?:\.|@)[a-zA-Z\d]{2,})|localhost)(?:(?:[-a-zA-Z\d:%_+.~#!?&//=@]*)(?:[,](?![\s]))*)*)/g);
+const defaultUrlRegex = () => (/((?:https?(?::\/\/))(?:www\.)?(?:[a-zA-Z\d-_.]+(?:(?:\.|@)[a-zA-Z\d]{2,})|localhost)(?:(?:[-a-zA-Z\d:%_+.~#!?&//=@]*)(?:[,](?![\s]))*)*)/g);
 
 // Get <a> element as string
 const linkify = (href, options) => createHtmlElement({
@@ -17,11 +17,11 @@ const linkify = (href, options) => createHtmlElement({
 const domify = html => document.createRange().createContextualFragment(html);
 
 const getAsString = (input, options) => {
-	return input.replace(urlRegex(), match => linkify(match, options));
+	return input.replace(options.urlRegexp, match => linkify(match, options));
 };
 
 const getAsDocumentFragment = (input, options) => {
-	return input.split(urlRegex()).reduce((frag, text, index) => {
+	return input.split(options.urlRegexp).reduce((frag, text, index) => {
 		if (index % 2) { // URLs are always in odd positions
 			frag.appendChild(domify(linkify(text, options)));
 		} else if (text.length > 0) {
@@ -33,8 +33,18 @@ const getAsDocumentFragment = (input, options) => {
 };
 
 module.exports = (input, options) => {
+        if (options.customUrlRegexp) {
+            if (typeof (options.customUrlRegexp) === 'function') {
+                options.urlRegexp = options.customUrlRegexp();
+            }
+            if (typeof (options.customUrlRegexp) === 'string') {
+                options.urlRegexp = new RegExp(options.customUrlRegexp, 'g');
+            }
+        }
+
 	options = Object.assign({
 		attributes: {},
+                urlRegexp: defaultUrlRegex(),
 		type: 'string'
 	}, options);
 
